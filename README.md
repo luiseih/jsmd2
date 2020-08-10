3cb40868bfa8b8795e16682ab78ff79ce348ea81
Luis E. I. Herrera <me@luiseih.com>
2020-08-10 14:56:28 +1000

    Add PyCharm helper files for development.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # jsmd2
+
+Job Search Multiple Database 2 is a project to help people keep track of their job applications, company
+profiles, outcomes. Sort of a CRM for job searches.
+
--- jsmd2.py
+++ jsmd2.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
 
-import os.path
+from pathlib import Path
+import os
 import configparser
 
-if not os.path.isfile('.jsmd2/jsmd2.conf'):
-    config = configparser.ConfigParser()
-    config['DEFAULT'] = {'ServerAliveInternal': '45',
-                         'Compression': 'yes',
-                         'CompressionLevel': '9'}
-    config['bitbucket.org'] = {}
-    config['bitbucket.org']['User'] = 'hg'
-    config['topsecret.server.com'] = {}
-    topsecret = config['topsecret.server.com']
-    topsecret['Port'] = '50022'                # mutates the parser
-    topsecret['ForwardX11'] = 'no'             # same here
-    config['DEFAULT']['ForwardX11'] = 'yes'
-    with open('.jsmd2/jsmd2.conf', 'w') as configfile:
-        config.write(configfile)
+
+def main():
+
+    home = str(Path.home())
+    full_path = os.path.join(home, ".config")
+    if not os.path.exists(full_path):
+        os.mkdir(full_path)
+    else:
+        os.chdir(full_path)
+
+    if not os.path.isfile('jsmd2.conf'):
+        config = configparser.ConfigParser()
+        config['DEFAULT'] = {'ServerAliveInternal': '45',
+                             'Compression': 'yes',
+                             'CompressionLevel': '9'}
+        config['bitbucket.org'] = {}
+        config['bitbucket.org']['User'] = 'hg'
+        config['topsecret.server.com'] = {}
+        topsecret = config['topsecret.server.com']
+        topsecret['Port'] = '50022'                # mutates the parser
+        topsecret['ForwardX11'] = 'no'             # same here
+        config['DEFAULT']['ForwardX11'] = 'yes'
+        with open('jsmd2.conf', 'w') as configfile:
+            config.write(configfile)
+
+    
+
+if __name__ == '__main__':
+    main()
+
 
