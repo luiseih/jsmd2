0444444ff9a9fbd07c039ea467594179c1d4a77f
Luis E. I. Herrera <me@luiseih.com>
2020-08-16 23:32:58 +1000

    Update jsmd2.py

--- jsmd2.py
+++ jsmd2.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+''' JSMD2 is an application to manage job applications.
+JSMD2 stands for Job Search Multi Document version 2.
+It acts as a front end for SQLite3.
+'''
+
+__author__    = "Luis E. I. Herrera"
+__copyright__ = "MIT License 2020"
+
 # TODO:
 #
 # Default app to see documents stored in
@@ -12,6 +20,7 @@
 import os
 import time
 import sqlite3
+import datetime
 import configparser
 from pathlib import Path
 
@@ -176,10 +185,15 @@ def main():
         return
 
     def menu_companies():
-        Menu_header()
+        menu_header()
         print("Companies")
         print()
-        print ("*"*30)
+        print("*"*30)
+        print()
+        print("Current companies are:")
+        company_read()
+        print()
+        print("*"*30)
         print("1. Add a new company.")
         print("2. Modify a company.")
         print("3. Delete a company.")
@@ -190,15 +204,66 @@ def main():
         print()
         choice = input("What would you like to do? ")
         if choice == "1":
-            pass
+            name = input("What is the company name? ")
+            address1 = input("What is the company address1? ")
+            address2 = input("What is the company address2? ")
+            city = input("What is the company city? ")
+            state = input("What is the company state? ")
+            if state == "":
+                state = "VIC"
+            postal_code = input("What is the company post code? ")
+            country = input("What is the company country? ")
+            if country == "":
+                country = "AUSTRALIA"
+            company_add(name, address1, address2, city, state, postal_code, country)
+            menu_companies()
         elif choice == "2":
-            pass
+            modify_company = input("What company would you like to modify? ")
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM company WHERE rowid=?', (modify_company,))
+            old_company = cur.fetchone()
+            print("What is the company name (",old_company[2],")? ")
+            name = input()
+            if name == "":
+                name = old_company[2]
+            print("What is the company address1 (",old_company[3],")? ")
+            address1 = input()
+            if address1 == "":
+                address1 = old_company[3]
+            print("What is the company address2 (",old_company[4],")? ")
+            address2 = input()
+            if address2 == "":
+                address2 = old_company[4]
+            print("What is the company city (",old_company[5],")? ")
+            city = input()
+            if city == "":
+                city = old_company[5]
+            print("What is the company state (",old_company[6],")? ")
+            state = input()
+            if state == "":
+                state = old_company[6]
+            print("What is the company post code (",old_company[7],")? ")
+            postal_code = input()
+            if postal_code == "":
+                postal_code = old_company[7]
+            print("What is the company country (",old_company[8],")? ")
+            country = input()
+            if country == "":
+                country = old_company[8]
+            company_modify(modify_company,name,address1,address2,city,state,postal_code,country)
+            menu_companies()
         elif choice == "3":
-            pass
+            delete_company = input("Company to delete? ")
+            company_delete(delete_company)
+            menu_companies()
         elif choice == "4":
-            pass
+            how_many = input("Last how many entries? ")
+            company_last_entries(how_many)
+            menu_companies()
         elif choice == "5":
-            pass
+            search_company = input("Company to search for? ")
+            company_search(search_company)
+            menu_companies()
         elif choice == "6":
             menu_main()
         elif choice == "7":
@@ -206,6 +271,51 @@ def main():
         else:
             menu_invalid_output()
             menu_companies()
+        return
+
+# Company menu header
+    def company_read():
+        for row in conn.execute('SELECT * FROM company;'):
+            print(row)
+        return
+       
+# Company menu option 1
+    def company_add(name, address1, address2, city, state, postal_code, country):
+        updated_on = datetime.datetime.now().isoformat()
+        conn.execute('INSERT INTO company \
+                (updated_on,name,address1,address2,city,state,postal_code,country) \
+                VALUES(?,?,?,?,?,?,?,?);', \
+                (updated_on,name,address1,address2,city,state,postal_code,country))
+        conn.commit()
+        return
+
+# Company menu option 2
+    def company_modify(modify_company,name,address1,address2,city,state,postal_code,country):
+        updated_on = datetime.datetime.now().isoformat()
+        conn.execute('UPDATE company SET \
+        updated_on=?,name=?,address1=?,address2=?,city=?,state=?,postal_code=?,country=? WHERE \
+        rowid=?;',(updated_on,name,address1,address2,city,state,postal_code,country,modify_company))
+        conn.commit()
+        return
+
+# Company menu option 3
+    def company_delete(delete_company):
+        conn.execute('DELETE FROM company WHERE rowid=?;', delete_company)
+        conn.commit()
+        return
+
+# Company menu option 4
+    def company_last_entries(how_many):
+        for row in conn.execute('SELECT * FROM company ORDER BY rowid DESC LIMIT ?;',how_many):
+            print(row)
+        input("Press ENTER to coninue...")
+        return
+
+# Company menu option 5
+    def company_search(search_company):
+        for row in conn.execute("SELECT * FROM company WHERE name LIKE ?;", ('%'+search_company+'%',)):
+            print(row)
+        input("Press ENTER to continue...")
         return
 
     def menu_jobs():
@@ -239,12 +349,17 @@ def main():
         choice = input("What would you like to do? ")
         if choice == "1":
             status = input("Type the new status: "),
-            status_write(status)
+            status_write_new(status)
             menu_status()
         elif choice == "2":
-            pass
+            old_status = input("Which option would you like to change? ")
+            new_status = input("What should the new option be? ")
+            status_replace(old_status, new_status)
+            menu_status()
         elif choice == "3":
-            pass
+            remove_status = input("Which option would you like to remove? ")
+            status_remove(remove_status)
+            menu_status()
         elif choice == "4":
             menu_main()
         elif choice == "5":
@@ -272,13 +387,27 @@ def main():
         def write():
             pass
 
+# Status menu header
     def status_read():
         for row in conn.execute('SELECT * FROM status;'):
             print(row)
         return
 
-    def status_write(status):
+# Status menu option 1
+    def status_write_new(status):
         conn.execute('INSERT INTO status(status) VALUES(?);', status)
+        conn.commit()
+        return
+
+# Status menu option 2
+    def status_replace(old_status, new_status):
+        conn.execute('UPDATE status SET status=? WHERE rowid=?;', (new_status, old_status))
+        conn.commit()
+        return
+
+# Status menu option 3
+    def status_remove(remove_status):
+        conn.execute('DELETE FROM status WHERE rowid=?;', remove_status)
         conn.commit()
         return
 
